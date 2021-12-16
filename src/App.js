4df8b8da2210dd1d389b08198f3a0e13ca66570f
goodmattg <mattgoodman13@gmail.com>
2021-12-16 10:22:45 -0800

    serious progress integrating mediapipe js, almost working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-import PreviewPort from './components/PreviewPort';
+import "./App.css";
+import PreviewPort from "./components/PreviewPort";
 
 function App() {
   return (
--- src/components/PreviewPort.js
+++ src/components/PreviewPort.js
@@ -1,40 +1,45 @@
-import * as React from 'react';
-import VideoUploadCard from './VideoUploadCard';
-import Box from '@mui/material/Box';
+import * as React from "react";
+import VideoUploadCard from "./VideoUploadCard";
+import Box from "@mui/material/Box";
 
-import Grid from '@mui/material/Grid';
-import LoadingButton from '@mui/lab/LoadingButton';
+import Grid from "@mui/material/Grid";
+import LoadingButton from "@mui/lab/LoadingButton";
 
 class PreviewPort extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            video_1: null,
-            video_2: null
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      video_1: null,
+      video_2: null,
+    };
+  }
 
-    uploadClick = () => {
-        console.log('Upload clicked');
-    }
-    render() {
-        return (
-            <Box sx={{ flexGrow: 1 }}>
-                <Grid container spacing={2} justifyContent="center">
-                    <Grid item xs={6}>
-                        <VideoUploadCard id="1"></VideoUploadCard>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <VideoUploadCard id="2"></VideoUploadCard>
-                    </Grid>
-                    <Grid item xs={12} md={8} lg={6}>
-                        <LoadingButton size="large" variant="outlined" onClick={this.uploadClick}>Align!</LoadingButton>
-                    </Grid>
-                </Grid>
-            </Box>
-        )
-    }
+  uploadClick = () => {
+    console.log("Upload clicked");
+  };
+  render() {
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item xs={6}>
+            <VideoUploadCard id="video-upload-1"></VideoUploadCard>
+          </Grid>
+          <Grid item xs={6}>
+            <VideoUploadCard id="video-upload-2"></VideoUploadCard>
+          </Grid>
+          <Grid item xs={12} md={8} lg={6}>
+            <LoadingButton
+              size="large"
+              variant="outlined"
+              onClick={this.uploadClick}
+            >
+              Align!
+            </LoadingButton>
+          </Grid>
+        </Grid>
+      </Box>
+    );
+  }
 }
-
 
 export default PreviewPort;
